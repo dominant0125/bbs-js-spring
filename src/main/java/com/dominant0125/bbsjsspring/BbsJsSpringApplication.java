c3315adbc1e928b74941221e69d9f1a6708e1ead
parkwoosung <abec0125@ncomz.com>
2019-12-26 18:13:41 +0900

    create new project

---
+++ src/main/java/com/dominant0125/bbsjsspring/BbsJsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dominant0125.bbsjsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsJsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsJsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dominant0125/bbsjsspring/BbsJsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominant0125.bbsjsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsJsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
